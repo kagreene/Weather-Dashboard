a0252b3b4ec9d23007a3a291559eb81339e60c20
kagreene <kristengreene2023@U.northwestern.edu>
2024-12-05 09:48:20 -0600

    fixed bugs that prevented application from running as intended

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -5,14 +5,26 @@ import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  console.log(req.body)
+  const cityName = req.body.cityName;
+  try {
+    const weatherData = await WeatherService.getWeatherForCity(cityName);
+    // TODO: save city to search history
+    await HistoryService.addCity(cityName)
 
-  // TODO: save city to search history
+    //Return weather data to client
+    res.json(weatherData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({error: 'Failed to retrieve weather data'})
+  }
+ 
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {
+router.get('/history', async (_req: Request, res: Response) => {
   try {
     const savedCities = await HistoryService.getCities();
     res.json(savedCities);
@@ -23,6 +35,6 @@ router.get('/history', async (req: Request, res: Response) => {
 });
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (_req: Request, _res: Response) => { });
 
 export default router;
--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -7,8 +7,7 @@ const router = Router();
 
 
 // TODO: Define route to serve index.html
-router.get('*', (req, res) =>
-{
+router.get('*', (_req, res) => {
     res.sendFile(path.join(__dirname, '../../client/index.html'))
 });
 export default router;

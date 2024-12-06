44244e5d306ab270e528ec604ebf641b51875937
kagreene <kristengreene2023@U.northwestern.edu>
2024-12-05 21:49:02 -0600

    bug fixes

--- server/dist/routes/htmlRoutes.js
+++ server/dist/routes/htmlRoutes.js
@@ -6,10 +6,6 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 // TODO: Define route to serve index.html
 router.get('*', (_req, res) => {
-<<<<<<<< HEAD:server/dist/routes/htmlRoutes.js
     res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
-========
-    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'))
->>>>>>>> e4e7597ccb2f98c9b5aa9583a7a00329ee6bb1f6:server/src/routes/htmlRoutes.ts
 });
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -9,11 +9,9 @@ import routes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 // TODO: Serve static files of entire client dist folder
-<<<<<<<< HEAD:server/dist/server.js
-app.use(express.static('../../client/dist'));
-========
+
 app.use(express.static( '../../client/dist'))
->>>>>>>> e4e7597ccb2f98c9b5aa9583a7a00329ee6bb1f6:server/src/server.ts
+
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 //Look at Activity 25 for middleware examples 
 
